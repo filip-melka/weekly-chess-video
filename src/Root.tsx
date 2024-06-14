cc86058e1789ff97a090bd7adea3c545a9616a1d
its-filip-melka <133359834+its-filip-melka@users.noreply.github.com>
2024-06-14 11:12:57 +0200

    test: add test data

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,6 +1,59 @@
 import { Composition } from 'remotion'
 import MyComposition from './Composition'
 
+const regularGames = {
+	weekNum: 1,
+	noOfGames: 5,
+	noOfWins: 2,
+	noOfDraws: 2,
+	noOfLosses: 1,
+	opponentAvatarURL:
+		'https://images.chesscomfiles.com/uploads/v1/user/342783555.cf63d771.200x200o.63c562ed1dc1.jpg',
+	opponentUsername: 'filipmelka24',
+	opponentRating: 896,
+	originalRating: 825,
+	newRating: 855,
+}
+
+const noAvatarImage = {
+	weekNum: 1,
+	noOfGames: 5,
+	noOfWins: 2,
+	noOfDraws: 2,
+	noOfLosses: 1,
+	opponentAvatarURL: null,
+	opponentUsername: 'filipmelka24',
+	opponentRating: 896,
+	originalRating: 825,
+	newRating: 855,
+}
+
+const noWinsGames = {
+	weekNum: 1,
+	noOfGames: 5,
+	noOfWins: 0,
+	noOfDraws: 2,
+	noOfLosses: 3,
+	opponentAvatarURL: null,
+	opponentUsername: null,
+	opponentRating: 0,
+	originalRating: 825,
+	newRating: 855,
+}
+
+const noGames = {
+	weekNum: 1,
+	noOfGames: 0,
+	noOfWins: 0,
+	noOfDraws: 0,
+	noOfLosses: 0,
+	opponentAvatarURL: null,
+	opponentUsername: null,
+	opponentRating: 896,
+	originalRating: 825,
+	newRating: 855,
+}
+
 export const RemotionRoot: React.FC = () => {
 	return (
 		<>
@@ -11,19 +64,7 @@ export const RemotionRoot: React.FC = () => {
 				fps={30}
 				width={1200}
 				height={674}
-				defaultProps={{
-					weekNum: 1,
-					noOfGames: 5,
-					noOfWins: 2,
-					noOfDraws: 2,
-					noOfLosses: 1,
-					opponentAvatarURL:
-						'https://cdn.hashnode.com/res/hashnode/image/upload/v1704265966452/HqKDXjGgI.png?w=500&h=500&fit=crop&crop=faces&auto=compress,format&format=webp',
-					opponentUsername: 'filipmelka24',
-					opponentRating: 896,
-					originalRating: 825,
-					newRating: 855,
-				}}
+				defaultProps={noAvatarImage}
 			/>
 		</>
 	)
