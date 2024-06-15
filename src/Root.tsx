62c67cb6c31d3a3143a9ea909b5fde5763bdd2d6
its-filip-melka <133359834+its-filip-melka@users.noreply.github.com>
2024-06-15 16:03:43 +0200

    feat: fetch last week data

--- src/Root.tsx
+++ src/Root.tsx
@@ -2,57 +2,30 @@ import { Composition } from 'remotion'
 import MyComposition from './Composition'
 import { getData } from './utils/dataFetching.js'
 
-const regularGames = {
-	weekNum: 1,
-	noOfGames: 5,
-	noOfWins: 2,
-	noOfDraws: 2,
-	noOfLosses: 1,
-	opponentAvatarURL:
-		'https://images.chesscomfiles.com/uploads/v1/user/342783555.cf63d771.200x200o.63c562ed1dc1.jpg',
-	opponentUsername: 'filipmelka24',
-	opponentRating: 896,
-	originalRating: 825,
-	newRating: 855,
-}
-
-const noAvatarImage = {
-	weekNum: 15,
-	noOfGames: 5,
-	noOfWins: 2,
-	noOfDraws: 2,
-	noOfLosses: 1,
-	opponentAvatarURL: null,
-	opponentUsername: 'filipmelka24',
-	opponentRating: 896,
-	previousRating: 825,
-	newRating: 855,
-}
-
-const noWinsGames = {
-	weekNum: 9,
-	noOfGames: 5,
-	noOfWins: 0,
-	noOfDraws: 2,
-	noOfLosses: 3,
-	opponentAvatarURL: null,
-	opponentUsername: null,
-	opponentRating: 0,
-	previousRating: 825,
-	newRating: 813,
-}
-
-const noGames = {
-	weekNum: 5,
+const defaultProps = {
+	weekNum: 0,
 	noOfGames: 0,
 	noOfWins: 0,
 	noOfDraws: 0,
 	noOfLosses: 0,
 	opponentAvatarURL: null,
 	opponentUsername: null,
-	opponentRating: 896,
-	previousRating: 825,
-	newRating: 825,
+	opponentRating: 0,
+	previousRating: 0,
+	newRating: 0,
+}
+
+type WeeklyData = {
+	weekNum: number
+	noOfGames: number
+	noOfWins: number
+	noOfDraws: number
+	noOfLosses: number
+	opponentAvatarURL: string | null
+	opponentUsername: string | null
+	opponentRating: number
+	previousRating: number
+	newRating: number
 }
 
 export const RemotionRoot: React.FC = () => {
@@ -65,17 +38,21 @@ export const RemotionRoot: React.FC = () => {
 				fps={30}
 				width={1200}
 				height={674}
-				defaultProps={noGames}
-				calculateMetadata={async ({ props }) => {
-					const data = await getData(new Date('2024-06-15'), 'filipmelka24')
-					console.log(data)
-					// ---
+				defaultProps={defaultProps}
+				calculateMetadata={async () => {
+					const lastWeekDate = new Date(
+						new Date().valueOf() - 7 * 24 * 3600 * 1000
+					)
+
+					const data: WeeklyData = await getData(lastWeekDate, 'filipmelka24')
+
 					let durationInFrames = 820
-					if (props.noOfGames === 0) durationInFrames = 500
-					else if (props.noOfWins === 0) durationInFrames = 620
+					if (data.noOfGames === 0) durationInFrames = 500
+					else if (data.noOfWins === 0) durationInFrames = 620
 
 					return {
 						durationInFrames,
+						props: data,
 					}
 				}}
 			/>
